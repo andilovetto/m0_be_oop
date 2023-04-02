9025c6acf772322d8ec83bb3fc92297d4d3ff7f0
andi lovetto <andilovetto@gmail.com>
2023-04-02 16:37:05 -0600

    Complete oop assignment and self eval

--- oop.rb
+++ oop.rb
@@ -2,15 +2,49 @@
 # it should have a dynamic name attribute
 # it should have a color attribute, that is silver by default
 # it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
+require 'pry'
+class Unicorn
+    attr_reader :name, :color
+    def initialize (name, color = "silver")
+        @name = name
+        @color = color
+    end
+    def say(string)
+        "*~*#{string}*~*"
+    end
+end
 
+unicorn1 = Unicorn.new("sally")
+p unicorn1.name
+p unicorn1.color
 
+p unicorn1.say("silly rabbit, unicorns are for kids!")
 
 #  Write a class called Vampire
 #  it should have a dynamic name attribute
 #  it should have a pet attribute, that is a bat, by default BUT it could be dynamic if info is passed in initially
 #  it should have a thirsty attribute, that is true by default
 #  it should have a drink method. When called, the thirsty attribute changes to false
+class Vampire
+    attr_reader :name, :pet, :thirsty
+    def initialize (name, pet = "bat", thirsty = true)
+        @name = name
+        @pet = pet
+        @thirsty = thirsty
+    end
+    def drink
+        @thirsty = false
+    end
+end
 
+vampire1 = Vampire.new("Vlad")
+p 'vampire'
+p vampire1.name
+p vampire1.pet
+p vampire1.thirsty
+
+p vampire1.drink
+p vampire1.thirsty
 
 #  Write a Dragon class
 #  it should have a dynamic name attribute (string)
@@ -19,6 +53,32 @@
 #  it should have a is_hungry attribute that is true by default
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
 
+class Dragon
+    attr_reader :name, :rider, :color, :hungry, :stomach_level
+    def initialize (name, rider, color, hungry = true, stomach_level = 0)
+        @name = name
+        @rider = rider
+        @color = color
+        @hungry = hungry
+        @stomach_level = stomach_level
+    end
+    def eat
+        @stomach_level += 1
+        if @stomach_level >= 4
+            @hungry = false
+        end
+    end
+end
+
+dragon1 = Dragon.new("Sam", "Jon", "blue")
+p 'dragon'
+p dragon1.hungry
+
+dragon1.eat
+dragon1.eat
+dragon1.eat
+dragon1.eat
+p dragon1.hungry
 
 #  Write a Hobbit class
 #  it should have a dynamic name attribute (string)
@@ -28,3 +88,50 @@
 #  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
 #  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+
+class Hobbit
+    attr_reader :name, :disposition, :age, :adult, :old, :has_ring
+    def initialize (name, disposition, age = 0, adult = false, old = false, has_ring = false)
+        @name = name
+        @disposition = disposition
+        @age = age
+        @adult = adult
+        @old = old
+        @has_ring = has_ring
+    end
+    
+    def celebrate_birthday
+        @age += 1
+        if @age >= 33 
+            @adult = true
+        end
+        if @age >= 101
+            @old = true
+        end
+    end
+
+    def has_ring
+        if @name == "Frodo" 
+            @has_ring = true
+        end
+    end
+end
+
+hobbit1 = Hobbit.new("Fredi", "sad")
+p "hobbit"
+p hobbit1.name
+p hobbit1.disposition
+
+p hobbit1.adult
+
+33.times do 
+    hobbit1.celebrate_birthday
+end
+p hobbit1.adult
+
+p hobbit1.age
+80.times do 
+    hobbit1.celebrate_birthday
+end
+p hobbit1.age
+p hobbit1.old
--- self-eval.md
+++ self-eval.md
@@ -1,5 +1,5 @@
 # Self-Evaluation
 
-- This final CFU encompassed everything you've learned in Mod 0, and maybe even required some learning outside of what you've explicitly been taught. How did it feel as you faced these challenges?
-- How is your workflow going, in terms of taking breaks, staying focused, etc.?
-- What did you learn from this exercise that you want to remember as you begin Mod 1?
+- This final CFU encompassed everything you've learned in Mod 0, and maybe even required some learning outside of what you've explicitly been taught. How did it feel as you faced these challenges? fucking awesome!!!! I loved the challenge and enjoyed putting all the pieces together.
+- How is your workflow going, in terms of taking breaks, staying focused, etc.? Should do better about taking breaks as it will help with staying focused
+- What did you learn from this exercise that you want to remember as you begin Mod 1? Not to be so hard on myself. I know it takes repetition and practice before feeling confident with new concepts and this all comes with time. 
